0d124ebe222ba4c420165af56abd88b246c3a568
robyrob78 <70548135+robyrob78@users.noreply.github.com>
2020-11-12 20:43:16 -0800

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,4 +1,4 @@
----------------Challenge Start-------------------------------------
+---------------Challenge Start---------------------------------------
 
 SELECT e.emp_no,
 e.first_name,
@@ -32,3 +32,21 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT (*) DESC
+---------------------------------------------------------------------
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO Mentorship_Eligibility 
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
